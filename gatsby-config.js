a7041359bc5a6b564c613a9cf013811024358501
wslyvh <wslyvh@users.noreply.github.com>
2020-03-06 13:03:33 +0100

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `wslyvh`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Wesley van Heije`,
+      summary: `who is building products and teams. Based in Amsterdam.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `A personal blog.`,
+    siteUrl: `https://wslyvh.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `wslyvh`,
     },
   },
   plugins: [
