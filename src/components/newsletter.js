040260bbf8a6cd59e5531b78a077f405292c09d9
wslyvh <wslyvh@users.noreply.github.com>
2020-08-04 13:20:18 +0200

    newsletter

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -2,18 +2,18 @@ import React from "react"
 
 const Newsletter = () => {
   return (
-    <>
+    <div className="m-2">
         <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css" />
 
         <h4 className="text-gray-800 text-xl my-2">Newsletter</h4>
         <p>Subscribe to stay up-to-date and get all the latest news and updates directly to your inbox. I'll never spam you!</p>
 
         <div id="mc_embed_signup">
-            <form action="https://vanheije.us12.list-manage.com/subscribe/post?u=ad3d60f21465218a2b99d8b40&amp;id=55e8df606c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+            <form action="https://vanheije.us12.list-manage.com/subscribe/post?u=ad3d60f21465218a2b99d8b40&amp;id=55e8df606c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
                 <div id="mc_embed_signup_scroll">
                     <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required />
                     <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
-                        <input type="text" name="b_ad3d60f21465218a2b99d8b40_55e8df606c" tabindex="-1" value="" />
+                        <input type="text" name="b_ad3d60f21465218a2b99d8b40_55e8df606c" tabIndex="-1" value="" />
                     </div>
                     <div className="clear ml-2">
                         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="bg-blue-600 hover:bg-blue-800 text-white py-1 px-4 rounded-full" />
@@ -21,7 +21,7 @@ const Newsletter = () => {
                 </div>
             </form>
         </div>
-    </>
+    </div>
   )
 }
 
